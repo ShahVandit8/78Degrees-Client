2e29015eda3cb5b632acaee66807f66d56691d5e
Vandit Shah <shahvandit8@gmail.com>
2022-11-27 17:26:13 +0530

    Changed server from Heroku to Render.com

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = "https://cafemanagementsys.herokuapp.com/api";
+const URL = "https://seven8degrees.onrender.com/api";
 
 export const addUser = async (data) => {
     try {
